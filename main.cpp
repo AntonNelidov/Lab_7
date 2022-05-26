42e6b9a117e7951d5d77a19b65c818b552008e45
AntonNelidov <tedrey0404@gmail.com>
2022-05-26 16:16:23 +0200

    second update

--- main.cpp
+++ main.cpp
@@ -36,6 +36,27 @@ public:
 				m_mat[i][j] = mat.m_mat[i][j];
 	}
 
+	// Перегрузка оператора ввода
+	istream& operator>>(istream& in, Matrix& mat)
+	{
+		for (int i = 0; i < mat.m_n; i++)
+			for (int j = 0; j < mat.m_m; j++)
+				in >> mat.m_mat[i][j];
+		return in;
+	}
+
+	// Перегрузка оператора вывода
+	ostream& operator<<(ostream& out, const Matrix& mat)
+	{
+		out << "Matrix " << mat.m_n << "x" << mat.m_m << endl;
+		for (int i = 0; i < mat.m_n; i++) {
+			for (int j = 0; j < mat.m_m; j++)
+				out << mat.m_mat[i][j] << " ";
+			out << endl;
+		}
+		return out;
+	}
+
 	// Присваивание
 	Matrix& operator=(const Matrix& mat)
 	{
